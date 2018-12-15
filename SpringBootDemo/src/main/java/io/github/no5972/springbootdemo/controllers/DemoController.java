0de95ad438b13cc2a9d46648ebed21f9cfd04979
No5972 <wujiuqier@foxmail.com>
2018-12-16 01:34:16 +0800

    add springbootdemo

---
+++ SpringBootDemo/src/main/java/io/github/no5972/springbootdemo/controllers/DemoController.java
@@ -0,0 +1,38 @@
+package io.github.no5972.springbootdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.github.no5972.springbootdemo.models.OperationResult;
+
+@Controller
+public class DemoController {
+	@RequestMapping("/springbootdemo/getResult")
+	public @ResponseBody OperationResult getResult() {
+		OperationResult result = new OperationResult();
+		result.setCode(0);
+		result.setMessage("test");
+		return result;
+	}
+	
+	@RequestMapping("/springbootdemo/getAnotherResult")
+	public String getAnotherResult(ModelMap model) {
+		model.addAttribute("result", "test");
+		return "result";
+	}
+	
+	@RequestMapping("/springbootdemo/login1")
+	public @ResponseBody OperationResult getLogin(String username, String password) {
+		OperationResult result = new OperationResult();
+		if (username.equals("wujiuqier") && password.equals("password")) {
+			result.setCode(0);
+			result.setMessage("success");
+		} else {
+			result.setCode(-1);
+			result.setMessage("username or password is incorrect");
+		}
+		return result;
+	}
+}
---
+++ SpringBootDemo/src/main/java/io/github/no5972/springbootdemo/models/OperationResult.java
@@ -0,0 +1,19 @@
+package io.github.no5972.springbootdemo.models;
+
+public class OperationResult {
+	private int code;
+	private String message;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+}
