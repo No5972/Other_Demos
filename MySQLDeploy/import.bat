62c1c8cc1293cd2865ea93df2b4a5bdeaa4d7b11
No.5972 <32865305+No5972@users.noreply.github.com>
2020-09-22 13:57:35 +0800

    Update import.bat

--- MySQLDeploy/import.bat
+++ MySQLDeploy/import.bat
@@ -1,6 +1,12 @@
 @echo off
 set input=
 set /p input=输入数据库名称:
+set username=
+set /p username=输入数据库用户名:
+set "psCommand=powershell -Command "$pword = read-host '输入数据库密码' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set password=%%p
 echo.>init.sql 
 del /F init.sql
 echo 删除临时SQL完成
@@ -10,7 +16,7 @@ echo 已生成临时SQL
 echo 开始创建数据库
 mysql -h localhost -uroot -proot < .\init.sql 2>&1
 echo 开始导入SQL
-mysql -h localhost -uroot -proot %input% < .\%input%.sql 2>&1
+mysql -h localhost -u%username% -p%password% %input% < .\%input%.sql 2>&1
 echo 删除临时SQL
 del /F init.sql
 echo 导入数据库完成
